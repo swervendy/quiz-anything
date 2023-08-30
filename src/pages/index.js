d45967a374505f60f643e01fd6d27d1be8f868d0
Aaron Cohen <talksay@Aarons-MacBook-Pro.local>
2023-08-30 21:31:57 +0100

    changed routes to make choose topic the home page. renamed files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,52 @@
 import { useState } from 'react';
-import { shuffleArray } from '@/lib/util';
-import ButtonAnswer from '@/components/ButtonAnswer';
-import triviaQuestions from '@/data/questions.json';
+import { useRouter } from 'next/router';
 
-export default function Home({ questions }) {
-  const [gameStatus, setGameStatus] = useState('playing');
-  const [score, setScore] = useState(0);
-  const [questionIndex, setQuestionIndex] = useState(0);
+function ChooseTopic() {
+  const [topic, setTopic] = useState('');
+  const router = useRouter();
 
-  const totalQuestions = questions.length;
-  const question = questions[questionIndex];
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  function startOver() {
-    setGameStatus('playing');
-    setScore(0);
-    setQuestionIndex(0);
-  }
+    try {
+      const response = await fetch('/api/generateQuestions', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ topic }),
+      });
+
+      if (response.ok) {
+        router.push('/quiz'); // Redirect to the quiz page
+      } else {
+        console.error('Failed to generate questions.');
+      }
+    } catch (error) {
+      console.error('Failed to generate questions:', error);
+    }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="z-10 w-full max-w-xl m-auto items-center justify-between px-8 lg:flex">
-        {gameStatus === 'finished' && (
-          <div className="w-full">
-            <h2 className="text-3xl text-center font-bold mb-4">Here&apos;s how you did!</h2>
-            <p className="text-xl text-center font-bold mb-12">
-              Your score was { score } / { totalQuestions }
-            </p>
-            <p className="text-xl text-center font-bold mb-12">
-              <button onClick={startOver}>Start Over</button>
-            </p>
-          </div>
-        )}
-        {gameStatus === 'playing' && (
-          <div className="w-full">
-            <h2 className="text-3xl text-center font-bold mb-4">Q: { question.question }</h2>
-            <h3 className="text-xl text-center font-bold mb-12">Score: { score } / { totalQuestions }</h3>
-            <ul className="grid grid-cols-2 w-full gap-4">
-              {question.answers.map(answer => {
-                function handleOnClick(e) {
-                  e.preventDefault();
-                  if (answer === question.answer) {
-                    setScore(prev => prev + 1);
-                  }
-                  if (questionIndex + 1 === questions.length) {
-                    setGameStatus('finished');
-                  } else {
-                    setQuestionIndex(prev => prev + 1);
-                  }
-                }
-                return (
-                  <li key={answer}>
-                    <ButtonAnswer onClick={handleOnClick}>
-                      { answer }
-                    </ButtonAnswer>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+        <h1 className="text-3xl text-center font-bold mb-4">Choose a Quiz Topic</h1>
+        <form onSubmit={handleSubmit} className="w-full">
+          <input
+            type="text"
+            placeholder="Enter a topic"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+            className="w-full p-2 border rounded mb-4"
+          />
+          <button type="submit" className="w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-4 px-16 rounded">
+            Start Quiz
+          </button>
+        </form>
       </div>
     </main>
   );
 }
 
-export async function getServerSideProps() {
-  return {
-    props: {
-      questions: triviaQuestions.map(q => ({
-        ...q,
-        answers: shuffleArray([q.answer, ...q.wrongAnswers])
-      }))
-    }
-  };
-}
+export default ChooseTopic;
+
