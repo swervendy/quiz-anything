8df7f1f06a1c94f223be4e7a909a266dc1e196ca
Aaron Cohen <aaronroicohen@gmail.com>
2023-09-13 02:28:30 +0100

    updated selector ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -108,47 +108,56 @@ function Index() {
     }
 };
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-start pt-20">
-      <h1 className="text-4xl font-bold mb-3">Quiz Anything</h1>
-      <p className="text-xl mb-6">Make a quiz about any topic or from a YouTube video</p>
-      <div className="tabs">
-        <button onClick={() => setTab('topic')}>Topic</button>
-        <button onClick={() => setTab('youtube')}>YouTube</button>
-      </div>
-      <div className="z-10 w-full max-w-xl m-auto items-center justify-between px-8 lg:flex">
-        <form onSubmit={handleSubmit} className="w-full">
-          {tab === 'topic' ? (
-            <input
-              type="text"
-              placeholder="Enter a topic"
-              value={topic}
-              onChange={(e) => setTopic(e.target.value)}
-              className="w-full p-2 border rounded mb-4"
-            />
+return (
+  <main className="flex min-h-screen flex-col items-center justify-start pt-20">
+    <h1 className="text-4xl font-bold mb-3">Quiz Anything</h1>
+    <p className="text-xl mb-6">Make a quiz about any topic or from a YouTube video</p>
+    <div className="tabs mb-4">
+      <button 
+        onClick={() => setTab('topic')}
+        className={`px-4 py-2 mr-2 rounded ${tab === 'topic' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+      >
+        Topic
+      </button>
+      <button 
+        onClick={() => setTab('youtube')}
+        className={`px-4 py-2 rounded ${tab === 'youtube' ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+      >
+        YouTube
+      </button>
+    </div>
+    <div className="z-10 w-full max-w-xl m-auto items-center justify-between px-8 lg:flex">
+      <form onSubmit={handleSubmit} className="w-full">
+        {tab === 'topic' ? (
+          <input
+            type="text"
+            placeholder="Enter a topic"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+            className="w-full p-2 border rounded mb-4"
+          />
+        ) : (
+          <input
+            type="text"
+            placeholder="Enter a YouTube URL"
+            value={youtubeURL}
+            onChange={(e) => setYoutubeURL(e.target.value)}
+            className="w-full p-2 border rounded mb-4"
+          />
+        )}
+        <button type="submit" disabled={isLoading} className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-4 px-16 rounded">
+          {isLoading ? (
+            <>
+              <div className="loader inline-block mr-2"></div>
+              Building your quiz! This may take a sec...
+            </>
           ) : (
-            <input
-              type="text"
-              placeholder="Enter a YouTube URL"
-              value={youtubeURL}
-              onChange={(e) => setYoutubeURL(e.target.value)}
-              className="w-full p-2 border rounded mb-4"
-            />
+            'Start Quiz'
           )}
-          <button type="submit" disabled={isLoading} className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-4 px-16 rounded">
-            {isLoading ? (
-              <>
-                <div className="loader inline-block mr-2"></div>
-                Building your quiz! This may take a sec...
-              </>
-            ) : (
-              'Start Quiz'
-            )}
-          </button>
-        </form>
-      </div>
-    </main>
-  );
-}
+        </button>
+      </form>
+    </div>
+  </main>
+)};
 
 export default Index;
