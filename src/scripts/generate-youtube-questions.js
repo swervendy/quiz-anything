450a4fb811109b19371d2f7e55ffaa83d7132577
Aaron Cohen <aaronroicohen@gmail.com>
2023-09-13 23:20:08 +0100

    added chunking to handle larger youtube transcripts

--- src/scripts/generate-youtube-questions.js
+++ src/scripts/generate-youtube-questions.js
@@ -16,19 +16,42 @@ const shape = [{
 export const generateYoutubeTriviaQuestions = async (transcriptArray = []) => {
   const transcriptText = transcriptArray.map(item => item.text).join(' ');
 
-  const completion = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo-16k",
-    messages: [{
-      role: "user", 
-      content: `
-        generate 10 trivia questions based on this YouTube transcript: ${transcriptText}. 
-        include wrong answers
-        format the response as JSON in the shape of: ${JSON.stringify(shape)}
-      `
-    }],
-  });
+  // Split the transcript into chunks of approximately 500 words each
+  const chunks = transcriptText.split(/\s+/).reduce((acc, word, i) => {
+    if (i % 500 === 0) acc.push('');
+    acc[acc.length - 1] += ' ' + word;
+    return acc;
+  }, []);
 
-  const questions = JSON.parse(completion.choices[0].message.content);
+  let questions = [];
+  let counter = 0;
+
+  for (const chunk of chunks) {
+    try {
+      const completion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo-16k",
+        messages: [{
+          role: "user", 
+          content: `
+            generate 10 trivia questions in English based on this YouTube transcript: ${chunk}. 
+            The questions should be related to the content of the transcript.
+            include wrong answers
+            format the response as JSON in the shape of: ${JSON.stringify(shape)}
+          `
+        }],
+      });
+
+      console.log(completion.choices[0].message.content);
+      const chunkQuestions = JSON.parse(completion.choices[0].message.content);
+      questions = [...questions, ...chunkQuestions];
+    } catch (error) {
+      console.error('Error during OpenAI API call:', error);
+    }
+
+    counter++;
+    if (counter >= 10) break; // Limit the number of iterations to 10
+  }
+
   return questions;
 };
 
@@ -38,4 +61,4 @@ if (require.main === module) {
       const questions = await generateYoutubeTriviaQuestions(transcriptArray);
       console.log(JSON.stringify(questions, null, 2));
     })();
-  }  
+}
