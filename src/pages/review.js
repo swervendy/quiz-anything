e8104786ee00dc1e68acd0e0fcaa5515cf771be4
Aaron Cohen <aaronroicohen@gmail.com>
2023-09-16 00:52:08 +0100

    cosmetic changes to reviews.js

--- src/pages/review.js
+++ src/pages/review.js
@@ -34,31 +34,31 @@ export default function Review() {
   }
 
   return (
-    <div>
+    <div className="p-6">
       <h2 className="text-3xl text-center font-bold mb-4">Here&apos;s how you did!</h2>
       <p className="text-xl text-center font-bold mb-12">
         Your score was {score} / {totalQuestions}
       </p>
-      <div className="flex justify-center space-x-4 mt-6">
+      <div className="flex justify-center space-x-4 mt-6 mb-8">
         <button 
           onClick={tryAgain} 
-          className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-4 rounded">
+          className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-6 rounded">
           Try this quiz again
         </button>
         <button 
           onClick={() => router.push('/')} 
-          className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-4 rounded">
+          className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-6 rounded">
           Create a new quiz
         </button>
       </div>
       {userAnswers && userAnswers.map((item, index) => (
-        <div key={index}>
-          <h2>Question {index + 1}</h2>
-          <p>{item.question}</p>
-          <p>Your answer: {item.userAnswer}</p>
+        <div key={index} className="mb-6 p-4 border rounded shadow">
+          <h2 className="text-2xl mb-4">Question {index + 1}</h2>
+          <p className="mb-2">{item.question}</p>
+          <p className="mb-4">Your answer: {item.userAnswer}</p>
           <button 
             onClick={() => router.push(`/tutor?question=${encodeURIComponent(item.question)}&answer=${encodeURIComponent(item.userAnswer)}`)} 
-            className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-4 rounded">
+            className="flex justify-center items-center w-full h-full text-lg uppercase font-bold hover:text-slate-600 bg-white dark:bg-slate-500 hover:bg-yellow-300 border-4 border-cyan-300 hover:border-yellow-500 py-2 px-6 rounded">
             Chat with a Tutor
           </button>
         </div>
