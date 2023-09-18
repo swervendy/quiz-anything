5aec1d39b3103946d1e51901eddb3cd6c5bffcb6
Aaron Cohen <aaronroicohen@gmail.com>
2023-09-18 22:15:04 +0100

    press enter to send messages

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -13,6 +13,13 @@ const MessageForm = () => {
     setContent('')
   }
 
+  const handleKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      handleSubmit();
+    }
+  }
+
   return (
     <form className="relative mx-auto max-w-3xl rounded-t-xl" onSubmit={handleSubmit}>
       <div className=" border-gray-200 h-[130px] rounded-t-xl backdrop-blur border-t border-l border-r border-gray-500/10 dark:border-gray-50/[0.06] bg-white supports-backdrop-blur:bg-white/95 p-5">
@@ -27,6 +34,7 @@ const MessageForm = () => {
           autoFocus
           className="!p-3 text-gray-900 border-0 ring-1 dark:ring-0 ring-gray-300/40 focus:ring-gray-300/80 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800/80 backdrop-blur shadow-none"
           onChange={(e: any) => setContent(e.target.value)}
+          onKeyPress={handleKeyPress}
         />
         <div className="absolute right-8 bottom-10">
           <div className="flex space-x-3">
@@ -43,4 +51,4 @@ const MessageForm = () => {
   )
 }
 
-export default MessageForm
+export default MessageForm;
